d9d8d8bf33fbc69ccacc47fb00435d9c2583460a
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2018-03-25 07:22:03 +0300

    Сделанheader & slider

---
+++ assets/template/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.autoplay').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
