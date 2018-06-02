f582c6bfd8fff62f3c6f0a85c4b499d53c363c34
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2018-06-02 08:40:38 +0300

    Добавление писателя

--- result_of_search.php
+++ result_of_search.php
@@ -12,16 +12,6 @@ foreach ($allWriter as $writer) {
     insertYearsInWriterTable($id_writer, $yearsOfLifeArray);
 }
 
-//$allWriter[] = $yearsOfLifeArray;
-//$allWriter = getAllWriter();
-//$allWriterPhoto = getAllMainWriterPicture();
-
-//$centuryId = getRequestParameter("authorCentury");
-//$countryId = getRequestParameter("authorCountry");
-//print_r($authorGenresIdsArray);
-
-//$allWriter = getFilterWriter($centuryId, $countryId, $authorGenresIdsArray);
-
 $currentFilter = '';
 getFilter('authorCentury', 'century.id_century', $currentFilter);
 getFilter('authorCountry', 'country.id_country', $currentFilter);
@@ -43,15 +33,8 @@ if ($idString)
 }
 
 $allWriter =  getFilterWriter($currentFilter);
-//echo $currentFilter;
-//print_r($allWriter);
-
-//print_r($authorGenres);
-//echo $centuryId . ' ' . $countryId;
 
 echo getView('result_of_search.twig', array(
     'allWriter' => $allWriter,
     'filterString' => $stringOfUserFilter
-//    'years' => $yearsOfLifeArray,
-//    'allPhoto' => $allWriterPhoto
 ));
